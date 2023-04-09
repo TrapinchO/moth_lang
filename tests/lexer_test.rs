e6f40e8152a5d1a403c24b93fa8cc9ab9d783a77
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-04-09 21:15:28 +0200

    tests: added some error tests

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -9,10 +9,19 @@ mod tests {
 
     #[test]
     fn lex_number() {
-        // TODO: errors
         let n = "10";
         let t = lex(n).unwrap();
         assert_eq!(t[0].typ, TokenType::Number(10))
+    }
+    #[test]
+    fn lex_number_err() {
+        let nums = [
+            ("1a", "Invalid digit: \"a\""),
+        ];
+        for (n, r) in nums {
+            let tok = lex(n);
+            assert_eq!(Err(r.to_string()), tok);
+        }
     }
 
     #[test]
@@ -34,7 +43,6 @@ mod tests {
 
     #[test]
     fn lex_string() {
-        // TODO: errors
         let strings = [
             ("\"\"", ""),
             ("\"test\"", "test"),
@@ -44,6 +52,17 @@ mod tests {
         for (s, r) in strings {
             let tok = lex(s).unwrap();
             assert_eq!(tok[0].typ, TokenType::String(r.to_string()));
+        }
+    }
+    #[test]
+    fn lex_string_err() {
+        let strings = [
+            ("\"", "EOF while parsing string"),
+            ("\"test\n\"", "EOL while parsing string"),
+        ];
+        for (s, e) in strings {
+            let tok = lex(s);
+            assert_eq!(Err(e.to_string()), tok);
         }
     }
 
