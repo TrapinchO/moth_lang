4579eebfa087073d41ab7825580697c176ebe189
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-04-12 22:51:13 +0200

    refactor: put lexer into a struct
    - some tests don't work
    - must finish "get_current" and other methods
    - a mess

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,37 +14,53 @@ pub enum TokenType {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Token {
-    pub pos: i32,
+    pub pos: usize,
     pub line: i32,
     pub typ: TokenType,
 }
 
 const SYMBOLS: &str = "+-*/=<>!|.$&@#";
 
-// TODO: eventually change into a struct
-pub fn lex(code: &str) -> Result<Vec<Token>, String> {
-    let mut tokens = vec![];
-    let chars: Vec<char> = code.chars().collect();
+struct Lexer {
+    code: Vec<char>,
+    idx: usize,
+    pos: usize,
+}
+impl Lexer {
+    pub fn new(code: &str) -> Self {
+        Self {
+            code: code.chars().collect(),
+            idx: 0,
+            pos: 0,
+        }
+    }
+    
+    fn get_current(&self) -> Result<char, String> {
+        if self.idx >= self.code.len() {
+            return Err("Attempted to index character out of bounds".to_string());
+        }
+        Ok(self.code[self.idx])
+    }
 
-    let mut idx = 0;
+    pub fn lex(&mut self) -> Result<Vec<Token>, String> {
+    let mut tokens = vec![];
+
     let mut line = 1;
-    let mut pos = 0;
-    while idx < code.len() {
-        let typ = match chars[idx] {
+    while self.idx < self.code.len() {
+        let typ = match self.get_current()? {
             ' ' => {
-                idx += 1;
+                self.idx += 1;
                 continue;
             }
             '\n' => {
                 line += 1;
-                pos = 0;
-                idx += 1;
+                self.pos = 0;
+                self.idx += 1;
                 continue;
             }
             num if num.is_digit(10) => {
-                let (num, idx2) = lex_number(&code[idx..])?;
-                idx += idx2 - 1;
-                pos += (idx2 - 1) as i32;
+                let (num, idx2) = self.lex_number()?;
+                self.pos += idx2 - 1;
                 TokenType::Number(num)
             },
             ident if ident.is_alphanumeric() => {
@@ -53,9 +69,8 @@ pub fn lex(code: &str) -> Result<Vec<Token>, String> {
                     ("fun", TokenType::Fun),
                 ].iter().cloned().collect();
 
-                let (ident, idx2) = lex_identifier(&code[idx..]);
-                idx += idx2 - 1;
-                pos += (idx2 - 1) as i32;
+                let (ident, idx2) = self.lex_identifier();
+                self.pos += idx2 - 1;
                 // NOTE: I am absolutely not sure about the map stuff
                 // but it works, so... yeah...
                 match keywords.get(&ident.as_str()) {
@@ -64,16 +79,15 @@ pub fn lex(code: &str) -> Result<Vec<Token>, String> {
                 }
             },
             sym if SYMBOLS.contains(sym) => {
-                let (sym, idx2) = lex_symbol(&code[idx..]);
-                idx += idx2 - 1;
-                pos += (idx2 - 1) as i32;
+                let (sym, idx2) = self.lex_symbol();
+                self.pos += idx2 - 1;
                 match sym.as_str() {
                     "=" => TokenType::Equals,
                     // ignore comments
                     // IMPLEMENTATION DETAIL: "//-" is an operator, not a comment
                     _ if sym.chars().all(|s| s == '/') && sym.len() >= 2 => {
-                        while idx < code.len() && chars[idx] != '\n' {
-                            idx += 1;
+                        while self.idx < self.code.len() && self.get_current().unwrap() != '\n' {
+                            self.idx += 1;
                         }
                         continue;
                     }
@@ -82,92 +96,89 @@ pub fn lex(code: &str) -> Result<Vec<Token>, String> {
             }
             // +1 to ignore the quote
             '\"' => {
-                let (string, idx2) = lex_string(&code[idx + 1..])?;
-                idx += idx2 - 1;
-                pos += (idx2 - 1) as i32;
+                let (string, idx2) = self.lex_string()?;
+                self.pos += idx2 - 1;
                 TokenType::String(string)
             },
             unknown => return Err(format!(
                     "Unknown character: \"{}\" at pos {} on line {}",
-                    unknown, pos, line
+                    unknown, self.pos, line
             ))
         };
-        tokens.push(Token { pos, line, typ });
-        idx += 1;
-        pos += 1;
+        tokens.push(Token { pos: self.pos, line, typ });
+        self.idx += 1;
+        self.pos += 1;
     }
 
     tokens.push(Token {
-        pos,
+        pos: self.pos,
         line,
         typ: TokenType::Eof,
     });
     Ok(tokens)
-}
+    }
 
-fn lex_number(code: &str) -> Result<(i32, usize), String> {
+fn lex_number(&mut self) -> Result<(i32, usize), String> {
     let mut num = String::from("");
 
-    let chars: Vec<char> = code.chars().collect();
-    let mut idx = 0;
-    while idx < code.len() {
-        if chars[idx].is_digit(10) {
-            num.push(chars[idx]);
-        } else if chars[idx].is_alphabetic() {
-            return Err(format!("Invalid digit: \"{}\"", chars[idx]));
+    while self.idx < self.code.len() {
+        if self.get_current().unwrap().is_digit(10) {
+            num.push(self.get_current().unwrap());
+        } else if self.get_current().unwrap().is_alphabetic() {
+            return Err(format!("Invalid digit: \"{}\"", self.get_current().unwrap()));
         } else {
             break;
         }
-        idx += 1;
+        self.idx += 1;
     }
-    Ok((num.parse::<i32>().unwrap(), idx))
+    Ok((num.parse::<i32>().unwrap(), self.idx))
 }
 
-fn lex_identifier(code: &str) -> (String, usize) {
+fn lex_identifier(&mut self) -> (String, usize) {
     let mut s = String::from("");
 
-    let chars: Vec<char> = code.chars().collect();
-    let mut idx = 0;
-    while idx < code.len() {
-        if !chars[idx].is_alphanumeric() {
+    while self.idx < self.code.len() {
+        if !self.get_current().unwrap().is_alphanumeric() {
             break;
         }
-        s.push(chars[idx]);
-        idx += 1;
+        s.push(self.get_current().unwrap());
+        self.idx += 1;
     }
-    (s, idx)
+    (s, self.idx)
 }
 
-fn lex_symbol(code: &str) -> (String, usize) {
+fn lex_symbol(&mut self) -> (String, usize) {
     let mut s = String::from("");
 
-    let chars: Vec<char> = code.chars().collect();
-    let mut idx = 0;
-    while idx < code.len() {
-        if !SYMBOLS.contains(chars[idx]) {
+    while self.idx < self.code.len() {
+        if !SYMBOLS.contains(self.get_current().unwrap()) {
             break;
         }
-        s.push(chars[idx]);
-        idx += 1;
+        s.push(self.get_current().unwrap());
+        self.idx += 1;
     }
-    (s, idx)
+    (s, self.idx)
 }
 
-fn lex_string(code: &str) -> Result<(String, usize), String> {
+fn lex_string(&mut self) -> Result<(String, usize), String> {
     let mut s = String::from("");
 
-    let chars: Vec<char> = code.chars().collect();
-    let mut idx = 0;
-    while idx < code.len() {
-        if chars[idx] == '\"' {
+    while self.idx < self.code.len() {
+        if self.get_current()? == '\"' {
             // +2 to move after the closing quote
-            return Ok((s, idx + 2));
+            self.idx += 2;
+            return Ok((s, self.idx + 2));
         }
-        if chars[idx] == '\n' {
+        if self.get_current()? == '\n' {
             return Err("EOL while parsing string".to_string());
         }
-        s.push(chars[idx]);
-        idx += 1;
+        s.push(self.get_current()?);
+        self.idx += 1;
     }
     Err("EOF while parsing string".to_string())
+}
+}
+
+pub fn lex(code: &str) -> Result<Vec<Token>, String> {
+    Lexer::new(code).lex()
 }
