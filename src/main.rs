5d154de856a5c4211afbebd7d791d04bbd7e8fef
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-04-22 21:29:45 +0200

    feat: added experimental binary expression parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use moth_lang::lexer;
-use moth_lang::parser::Expr;
+use moth_lang::parser;
 use std::env;
 
 fn main() {
     // courtesy of: https://stackoverflow.com/a/71731489
     env::set_var("RUST_BACKTRACE", "1");
 
-    let input = String::from("1 + 1 // - 2 *3a =\n+ \"Hello World!\" 123");
+    //let input = String::from("1 + 1 // - 2 *3a =\n+ \"Hello World!\" 123");
     //let input = String::from("hello /* fasd \n fsdf sd 4566 */ 1000a");
+    let input = String::from("1 + 1 - 1 * 2 / 2");
     let y = lexer::lex(&input);
     match y {
         Err(err) => {
@@ -21,21 +22,24 @@ fn main() {
             }
             let line = &lines[err.line];
             println!("{}", err.format_message(line));
+            return;
         }
         Ok(tokens) => {
             println!("===== source =====");
             println!("{:?}", input);
             println!("===== lexing =====");
-            for tok in tokens {
+            for tok in &tokens {
                 println!("{:?}", tok);
+            }
+
+            let ast = parser::parse(tokens);
+            match ast {
+                Err(err) => println!("[MOTH] Error: {}", err),
+                Ok(expr) => {
+                    println!("{:?}", expr);
+                }
             }
         }
     }
 
-    let ast = Expr::BinaryOperation(
-        "+".to_string(),
-        Box::new(Expr::Number(1)),
-        Box::new(Expr::Number(1)),
-    );
-    println!("{:?}", ast);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,62 @@
+use crate::lexer::{Token, TokenType};
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Expr {
     Number(i32),
     String(String),
     UnaryOperation(String, Box<Expr>),
-    BinaryOperation(String, Box<Expr>, Box<Expr>),
+    BinaryOperation(Box<Expr>, String, Box<Expr>),
+}
+
+pub fn parse(tokens: Vec<Token>) -> Result<Expr, String> {
+    Parser::new(tokens).parse()
+}
+
+
+struct Parser {
+    tokens: Vec<Token>,
+    idx: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            tokens,
+            idx: 0,
+        }
+    }
+    
+    pub fn parse(&mut self) -> Result<Expr, String> {
+        self.parse_expr()
+    }
+
+    fn parse_expr(&mut self) -> Result<Expr, String> {
+        println!("{:?}", self.tokens);
+        let mut left = self.parse_primary()?;
+        self.idx += 1;
+        while let Some(Token {typ: TokenType::Symbol(sym), .. }) = self.tokens.get(self.idx) {
+            self.idx += 1;
+            let right = self.parse_primary()?;
+            self.idx += 1;
+            left = Expr::BinaryOperation(
+                Box::new(left),
+                sym.clone(),
+                Box::new(right)
+            )
+        }
+        Ok(left)
+    }
+
+    fn parse_primary(&self) -> Result<Expr, String> {
+        let expr = match self.tokens.get(self.idx) {
+            None => Err("Expected an element".to_string()),
+            Some(tok) => Ok(match &tok.typ {
+                TokenType::String(s) => Expr::String(s.to_string()),
+                TokenType::Number(n) => Expr::Number(*n),
+                _ => return Err(format!("Unknown element: {:?}", tok)),
+            })
+        };
+        //self.idx += 1;
+        expr
+    }
 }
