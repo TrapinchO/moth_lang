60773f6c84c6845123d7181239bf38ae7b500141
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-04-27 22:42:14 +0200

    fix: fixed main function

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,14 @@ use std::env;
 fn main() {
     // courtesy of: https://stackoverflow.com/a/71731489
     env::set_var("RUST_BACKTRACE", "1");
-    match run() {
-        Ok(_) => {}
-        //Err(err) => println!("{:?}", err)
-        Err(err) => println!("{}", err.format_message("aaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
-    }
-}
-fn run() -> Result<(), Error> {
+
+
     //let input = String::from("1 + 1 // - 2 *3a =\n+ \"Hello World!\" 123");
     //let input = String::from("hello /* fasd \n fsdf sd 4566 */ 1000a");
-    let input = String::from("1 - 1 * 1 + 1");
-    println!("===== source =====\n{}", input);
-    match lexer::lex(&input) {
-        Err(err) => Err(err)?,
-            /*{
+    let input = String::from("(1 * 1 + 1) * 1 + 1");
+    match run(input.clone()) {
+        Ok(_) => {}
+        Err(err) => {
             let lines = input.split('\n').map(str::to_string).collect::<Vec<_>>();
             if lines.len() < err.line {
                 panic!(
@@ -29,24 +23,20 @@ fn run() -> Result<(), Error> {
                 );
             }
             let line = &lines[err.line];
-            Err(err.format_message(line))
-        }
-            */
-        Ok(tokens) => {
-            println!("===== lexing =====");
-            for t in &tokens {
-                println!("{:?}", t);
-            }
-            let ast = parser::parse(tokens)?;
-            println!("===== parsing =====\n{}", ast);
-            let resassoc = parser::reassoc(&ast).map_err(|msg| Error {
-                msg: msg,
-                line: 0,
-                start: 0,
-                end: 0
-            })?;
-            println!("===== reassociating =====\n{}", resassoc);
-            Ok(())
+            println!("{}", err.format_message(line));
         }
     }
+}
+fn run(input: String) -> Result<(), Error> {
+    println!("===== source =====\n{}\n=====        =====", input);
+    let tokens = lexer::lex(&input)?;
+    println!("===== lexing =====");
+    for t in &tokens {
+        println!("{:?}", t);
+    }
+    let ast = parser::parse(tokens)?;
+    println!("===== parsing =====\n{}", ast);
+    let resassoc = parser::reassoc(&ast)?;
+    println!("===== reassociating =====\n{}", resassoc);
+    Ok(())
 }
