65823011d60cdd752478a39337d65bfbfa876dd3
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-05-10 20:22:11 +0200

    feat: added a repl loop
    also trimmed the input because read_line gave newline at the end

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use moth_lang::error::Error;
 use moth_lang::lexer;
 use moth_lang::parser;
 use std::env;
+use std::io;
 
 fn main() {
     // courtesy of: https://stackoverflow.com/a/71731489
@@ -9,25 +10,31 @@ fn main() {
 
     //let input = String::from("1 + 1 // - 2 *3a =\n+ \"Hello World!\" 123");
     //let input = String::from("hello /* fasd \n fsdf sd 4566 */ 1000a");
-    let input = String::from("(1 * 1 + 1) * 1 + 1");
-    match run(input.clone()) {
-        Ok(_) => {}
-        Err(err) => {
-            let lines = input.split('\n').map(str::to_string).collect::<Vec<_>>();
-            if lines.len() < err.line {
-                panic!(
-                    "Error line ({}) is greater than the number of lines in the code ({})",
-                    err.line,
-                    lines.len()
-                );
+    //let input = String::from("(1 * 1 + 1) * 1 + 1");
+    loop {
+        println!("===== input =====");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        match run(input.clone()) {
+            Ok(_) => {}
+            Err(err) => {
+                let lines = input.split('\n').map(str::to_string).collect::<Vec<_>>();
+                if lines.len() < err.line {
+                    panic!(
+                        "Error line ({}) is greater than the number of lines in the code ({})",
+                        err.line,
+                        lines.len()
+                    );
+                }
+                let line = &lines[err.line];
+                println!("{}", err.format_message(line));
             }
-            let line = &lines[err.line];
-            println!("{}", err.format_message(line));
         }
     }
 }
 fn run(input: String) -> Result<(), Error> {
-    println!("===== source =====\n{}\n=====        =====", input);
+    println!("===== source =====\n{:?}\n=====        =====", input);
     let tokens = lexer::lex(&input)?;
     println!("===== lexing =====");
     for t in &tokens {
