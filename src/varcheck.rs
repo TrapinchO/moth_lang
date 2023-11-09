b1879bee41439de86be03210211955e71b335b4e
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2023-11-09 15:39:17 +0100

    experiment: more errors?

--- src/varcheck.rs
+++ src/varcheck.rs
@@ -17,8 +17,8 @@ pub fn varcheck(stmt: &Vec<Stmt>) -> Result<Vec<Stmt>, Error> {
 struct VarCheck;
 
 impl VarCheck {
-    fn check_block(&mut self, block: Vec<Stmt>) -> Result<(), Error> {
-        let mut vars = HashSet::new();
+    fn check_block(&mut self, block: &Vec<Stmt>) -> Result<(), Error> {
+        let mut vars: HashSet<String> = HashSet::new();
         for s in block {
             match &s.val {
                 StmtType::VarDeclStmt(Token { val: TokenType::Identifier(name) }, start: _, end: ..) => vars.insert(name),
@@ -43,60 +43,60 @@ impl VarCheck {
 impl StmtVisitor<Stmt> for VarCheck {
     // for these there is nothing to check (yet)
     fn var_decl(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     fn assignment(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     fn expr(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     // go through
     fn block(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
         let StmtType::BlockStmt(block) = &stmt.val else { unreachable!() };
         self.check_block(block)?;
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     fn if_else(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
         let StmtType::IfStmt(blocks) = &stmt.val else { unreachable!() };
         for block in blocks {
             self.check_block(block)?;
         }
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     fn whiles(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
         let StmtType::WhileStmt(cond, block) = &stmt.val else { unreachable!() };
         self.check_block(block)?;
-        Ok(stmt)
+        Ok(stmt.clone())
     }
     fn print(&mut self, stmt: &Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
+        Ok(stmt.clone())
     }
 }
 
 impl ExprVisitor<Expr> for VarCheck {
     fn int(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn float(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn string(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn bool(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn identifier(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn parens(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn unary(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
     fn binary(&mut self, expr: &Expr) -> Result<Expr, Error> {
-        Ok(expr)
+        Ok(expr.clone())
     }
 }
