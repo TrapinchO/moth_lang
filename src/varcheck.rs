d146e8f3b4c3506ac3bd0a0566e8ef81e0a4ce39
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2024-03-01 21:25:38 +0100

    refactor: radical return type changes to VarCheck; my Visitor is falling apart :(

--- src/varcheck.rs
+++ src/varcheck.rs
@@ -4,7 +4,6 @@ use crate::{
     exprstmt::*,
     token::*,
     value::*,
-    visitor::*,
 };
 
 use std::collections::{HashMap, HashSet};
@@ -15,7 +14,6 @@ pub fn varcheck(builtins: HashMap<String, ValueType>, stmt: Vec<Stmt>) -> Result
         errs: vec![],
     };
     var_check.check_block(stmt);
-    println!("{:?}", var_check.errs);
     if !var_check.errs.is_empty() {
         Err(var_check.errs)
     } else {
@@ -29,7 +27,7 @@ struct VarCheck {
 }
 
 impl VarCheck {
-    fn check_block(&mut self, block: Vec<Stmt>) -> Result<(), Error> {
+    fn check_block(&mut self, block: Vec<Stmt>) {
         self.env.add_scope();
         for s in block {
             match &s.val {
@@ -40,7 +38,7 @@ impl VarCheck {
                     },
                     expr,
                 ) => {
-                    self.visit_expr(expr.clone())?;
+                    self.visit_expr(expr.clone());
 
                     if self.env.contains(name) {
                         // TODO: functions behave weirdly
@@ -77,7 +75,7 @@ impl VarCheck {
                         Value { val: ValueType::Function(vec![], vec![]), start: 0, end: 0 }
                     ).unwrap();
 
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 },
                 StmtType::AssignStmt(
                     Token {
@@ -86,7 +84,7 @@ impl VarCheck {
                     },
                     expr,
                 ) => {
-                    self.visit_expr(expr.clone())?;
+                    self.visit_expr(expr.clone());
                     if !self.env.contains(name) {
                         self.errs.push(Error {
                             msg: "Undeclared variable".to_string(),
@@ -95,84 +93,93 @@ impl VarCheck {
                     }
                 }
                 StmtType::BlockStmt(..) => {
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
                 StmtType::IfStmt(..) => {
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
                 StmtType::WhileStmt(..) => {
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
                 StmtType::ExprStmt(..) => {
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
                 StmtType::BreakStmt => {},
                 StmtType::ContinueStmt => {},
                 StmtType::ReturnStmt(..) => {
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
                 _ => {
                     // TODO: this is like the classic meme "idk what it did so I removed it"...
                     // it broke.
-                    self.visit_stmt(s)?;
+                    self.visit_stmt(s);
                 }
             }
         }
         self.env.remove_scope();
-        Ok(())
+        
     }
 }
 
-impl StmtVisitor<Stmt> for VarCheck {
+impl VarCheck {
+    fn visit_stmt(&mut self, stmt: Stmt) {
+        match stmt.val {
+            StmtType::VarDeclStmt(..) => self.var_decl(stmt),
+            StmtType::AssignStmt(..) => self.assignment(stmt),
+            StmtType::ExprStmt(..) => self.expr(stmt),
+            StmtType::BlockStmt(..) => self.block(stmt),
+            StmtType::IfStmt(..) => self.if_else(stmt),
+            StmtType::WhileStmt(..) => self.whiles(stmt),
+            StmtType::FunDeclStmt(..) => self.fun(stmt),
+            StmtType::ContinueStmt => {},
+            StmtType::BreakStmt => {},
+            StmtType::ReturnStmt(..) => self.retur(stmt),
+        }
+    }
+
     // for these there is nothing to check (yet)
-    fn var_decl(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn var_decl(&mut self, stmt: Stmt) {
         let StmtType::VarDeclStmt(_, expr) = stmt.val.clone() else {
             unreachable!()
         };
-        self.visit_expr(expr)?;
-        Ok(stmt)
+        self.visit_expr(expr);
     }
-    fn assignment(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn assignment(&mut self, stmt: Stmt) {
         let StmtType::AssignStmt(_, expr) = stmt.val.clone() else {
             unreachable!()
         };
-        self.visit_expr(expr)?;
-        Ok(stmt)
+        self.visit_expr(expr);
     }
-    fn expr(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn expr(&mut self, stmt: Stmt) {
         let StmtType::ExprStmt(expr) = stmt.val.clone() else {
             unreachable!()
         };
-        self.visit_expr(expr)?;
-        Ok(stmt)
+        self.visit_expr(expr);
     }
     // go through
-    fn block(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn block(&mut self, stmt: Stmt) {
         let StmtType::BlockStmt(block) = stmt.val.clone() else {
             unreachable!()
         };
-        self.check_block(block)?;
-        Ok(stmt)
+        self.check_block(block);
     }
-    fn if_else(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn if_else(&mut self, stmt: Stmt) {
         let StmtType::IfStmt(blocks) = stmt.val.clone() else {
             unreachable!()
         };
         for (cond, block) in blocks {
-            self.visit_expr(cond)?;
-            self.check_block(block)?;
+            self.visit_expr(cond);
+            self.check_block(block);
         }
-        Ok(stmt)
     }
-    fn whiles(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn whiles(&mut self, stmt: Stmt) {
         let StmtType::WhileStmt(cond, block) = stmt.val.clone() else {
             unreachable!()
         };
-        self.visit_expr(cond)?;
-        self.check_block(block)?;
-        Ok(stmt)
+        self.visit_expr(cond);
+        self.check_block(block);
     }
-    fn fun(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn fun(&mut self, stmt: Stmt) {
         let StmtType::FunDeclStmt(_, params, block) = stmt.val.clone() else {
             unreachable!()
         };
@@ -192,42 +199,44 @@ impl StmtVisitor<Stmt> for VarCheck {
         self.env.add_scope_vars(
             params2.iter().map(|p| { (p.clone(), ValueType::Unit) }).collect::<HashMap<_, _>>()
         );
-        self.check_block(block)?;
+        self.check_block(block);
         self.env.remove_scope();
-        Ok(stmt)
     }
-    fn cont(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
-    }
-    fn brek(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
-        Ok(stmt)
-    }
-    fn retur(&mut self, stmt: Stmt) -> Result<Stmt, Error> {
+    fn retur(&mut self, stmt: Stmt) {
         let StmtType::ReturnStmt(expr) = stmt.val.clone() else {
             unreachable!()
         };
-        self.visit_expr(expr)?;
-        Ok(stmt)
+        self.visit_expr(expr);
     }
 }
 
-impl ExprVisitor<()> for VarCheck {
-    fn unit(&mut self, _: Expr) -> Result<(), Error> {
-        Ok(())
+impl VarCheck {
+    fn visit_expr(&mut self, expr: Expr) {
+        match &expr.val {
+            ExprType::Unit => self.unit(expr),
+            ExprType::Int(..) => self.int(expr),
+            ExprType::Float(..) => self.float(expr),
+            ExprType::String(..) => self.string(expr),
+            ExprType::Bool(..) => self.bool(expr),
+            ExprType::Identifier(..) => self.identifier(expr),
+            ExprType::Parens(..) => self.parens(expr),
+            ExprType::Call(..) => self.call(expr),
+            ExprType::UnaryOperation(..) => self.unary(expr),
+            ExprType::BinaryOperation(..) => self.binary(expr),
+        }
     }
-    fn int(&mut self, _: Expr) -> Result<(), Error> {
-        Ok(())
+
+    fn unit(&mut self, _: Expr) {
     }
-    fn float(&mut self, _: Expr) -> Result<(), Error> {
-        Ok(())
+    fn int(&mut self, _: Expr) {
     }
-    fn string(&mut self, _: Expr) -> Result<(), Error> {
-        Ok(())
+    fn float(&mut self, _: Expr) {
     }
-    fn bool(&mut self, _: Expr) -> Result<(), Error> {
-        Ok(())
+    fn string(&mut self, _: Expr) {
     }
-    fn identifier(&mut self, expr: Expr) -> Result<(), Error> {
+    fn bool(&mut self, _: Expr) {
+    }
+    fn identifier(&mut self, expr: Expr) {
         let ExprType::Identifier(name) = expr.val.clone() else {
             unreachable!()
         };
@@ -237,38 +246,33 @@ impl ExprVisitor<()> for VarCheck {
                 lines: vec![expr.loc()],
             });
         }
-        Ok(())
     }
-    fn parens(&mut self, expr: Expr) -> Result<(), Error> {
+    fn parens(&mut self, expr: Expr) {
         let ExprType::Parens(expr2) = expr.val else {
             unreachable!()
         };
-        self.visit_expr(*expr2)?;
-        Ok(())
+        self.visit_expr(*expr2);
     }
-    fn call(&mut self, expr: Expr) -> Result<(), Error> {
+    fn call(&mut self, expr: Expr) {
         let ExprType::Call(callee, args) = expr.val else {
             unreachable!()
         };
-        self.visit_expr(*callee)?;
+        self.visit_expr(*callee);
         for arg in args {
-            self.visit_expr(arg)?;
+            self.visit_expr(arg);
         }
-        Ok(())
     }
-    fn unary(&mut self, expr: Expr) -> Result<(), Error> {
+    fn unary(&mut self, expr: Expr) {
         let ExprType::UnaryOperation(_, expr2) = expr.val else {
             unreachable!()
         };
-        self.visit_expr(*expr2)?;
-        Ok(())
+        self.visit_expr(*expr2);
     }
-    fn binary(&mut self, expr: Expr) -> Result<(), Error> {
+    fn binary(&mut self, expr: Expr) {
         let ExprType::BinaryOperation(left, _, right) = expr.val else {
             unreachable!()
         };
-        self.visit_expr(*left)?;
-        self.visit_expr(*right)?;
-        Ok(())
+        self.visit_expr(*left);
+        self.visit_expr(*right);
     }
 }
