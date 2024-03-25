28e81094d066ac706ac31a3df0cdf64aa62ebdce
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2024-03-25 20:18:12 +0100

    refactor: removed "new" constructor

--- src/located.rs
+++ src/located.rs
@@ -13,11 +13,6 @@ pub struct Located<T> {
     pub loc: Location,
 }
 
-impl<T> Located<T> {
-    pub fn new(val: T, start: usize, end: usize) -> Self {
-        Located { val, loc: Location { start, end } }
-    }
-}
 
 impl<T: std::fmt::Display> Display for Located<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
