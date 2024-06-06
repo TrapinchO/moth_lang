5370bee5ed9698fe6c1f7c3bd14e24b4522c1866
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2024-06-06 10:51:46 +0200

    feat: added Display to lowexprstmt
    apparently useful for debugging

--- src/backend/lowexprstmt.rs
+++ src/backend/lowexprstmt.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 /// a lower representation of the AST
 /// as desugared and simplified as possible
 /// i.e. operators (both unary and binary) are changed into function calls
@@ -23,6 +25,39 @@ pub enum ExprType {
     Lambda(Vec<Identifier>, Vec<Stmt>), // |params| { block }
 }
 
+impl ExprType {
+    fn format(&self) -> String {
+        match self {
+            Self::Unit => "()".to_string(),
+            Self::Int(n) => n.to_string(),
+            Self::Float(n) => n.to_string(),
+            Self::String(s) => format!("\"{s}\""),
+            Self::Bool(b) => b.to_string(),
+            Self::Identifier(ident) => ident.to_string(),
+            Self::Call(callee, args) => format!(
+                "{callee}({args})",
+                args = args.iter().map(|e| { format!("{e}") }).collect::<Vec<_>>().join(", ")
+            ),
+            Self::List(ls) => format!(
+                "[{}]",
+                ls.iter().map(|e| { format!("{e}") }).collect::<Vec<_>>().join(", ")
+            ),
+            Self::Index(expr, idx) => format!("{}[{}]", expr.val, idx.val),
+            Self::Lambda(params, block) => format!(
+                "lambda({params}){block}",
+                params = params.iter().map(|s| s.to_string()).collect::<Vec<_>>().join(", "),
+                block = block.iter().map(|s| s.to_string()).collect::<Vec<_>>().join("\n")
+            ),
+        }
+    }
+}
+
+impl Display for ExprType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.format())
+    }
+}
+
 pub type Expr = Located<ExprType>;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -37,6 +72,53 @@ pub enum StmtType {
     ReturnStmt(Expr),
     BreakStmt,
     ContinueStmt,
+}
+
+impl StmtType {
+    fn format(&self) -> String {
+        match self {
+            Self::ExprStmt(expr) => expr.to_string() + ";",
+            Self::VarDeclStmt(ident, expr) => format!("let {ident} = {expr};"),
+            Self::AssignStmt(ident, expr) => format!("{ident} = {expr};"),
+            Self::AssignIndexStmt(ls, idx, val) => format!("{ls}[{idx}] = {val};"),
+            Self::BlockStmt(block) => format!(
+                "{{\n{block}\n}}",
+                block = block.iter().map(|s| s.to_string()).collect::<Vec<_>>().join("\n")
+            ),
+            Self::IfStmt(blocks) => {
+                let first = blocks.first().unwrap(); // always present
+                let rest = &blocks[1..]
+                    .iter()
+                    .map(|(cond, stmts)| {
+                        format!(
+                            "else if {cond} {{\n{stmts}\n}}",
+                            cond = cond.val,
+                            stmts = stmts.iter().map(|s| s.to_string()).collect::<Vec<_>>().join("\n")
+                        )
+                    })
+                    .collect::<Vec<_>>();
+                format!(
+                    "if {cond} {{\n{block}\n}} {rest}",
+                    cond = first.0,
+                    block = first.1.iter().map(|s| s.to_string()).collect::<Vec<_>>().join("\n"),
+                    rest = rest.join("")
+                )
+            }
+            Self::WhileStmt(cond, block) => format!(
+                "while {cond} {{{block}}}",
+                block = block.iter().map(|s| s.to_string()).collect::<Vec<_>>().join("\n")
+            ),
+            Self::ReturnStmt(expr) => format!("return {expr};"),
+            Self::BreakStmt => "break;".to_string(),
+            Self::ContinueStmt => "continue;".to_string(),
+        }
+    }
+}
+
+impl Display for StmtType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.format())
+    }
 }
 
 pub type Stmt = Located<StmtType>;
